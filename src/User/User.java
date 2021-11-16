30fc76ec02085ea15a6d062bf4830a6af2793e03
HassanMahmoud22 <hassan444mahmoud@gmail.com>
2021-11-16 20:58:26 +0200

    implemented login function

--- src/User/User.java
+++ src/User/User.java
@@ -17,14 +17,31 @@ public interface User {
      public String getMobileNumber();
      public Status getStatus();
 
-
-
      public void setUsername(String username);
      public void setEmail(String email);
      public void setPassword(String password);
      public void setStatus(Status status);
 
-     public default void Login(){
-          System.out.println("Welcome");
+     public default void Login(String username, String password)
+     {
+          Scanner scanner = new Scanner(System.in);
+          int queryResult= 0;
+          String query = "";
+          Connect C1 = new Connect();
+          C1.establish_connection();
+          Statement statement = C1.establish_connection().createStatement();
+          ResultSet rs = statement.executeQuery("select * from driver Where username = " + username + "and pass = " + password);
+          if(rs.next())
+               System.out.println("Logged in successfully");
+          else
+          {
+               rs = statement.executeQuery("select * from client Where username = " + username + "and pass = " + password);
+               if(rs.next())
+                    System.out.println("Logged in successfully");
+               else
+                    System.out.println("username or password is incorrect");
+
+          }
+          statement.close();
      }
  }
